5e1147f3b83ab5582f3bb36762457d8c23caec8e
BramLippens <bram.lippens09@gmail.com>
2023-11-01 21:00:14 +0100

    Day One

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -9,7 +9,55 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var lines = _input.Split('\n');
+        int sum = 0;
+        int counter = 1;
+        Dictionary<int, int> ElfCalories = new();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach (var line in lines)
+        {
+            if (int.TryParse(line, out int number))
+            {
+                sum += number;
+            }
+            else
+            {
+                ElfCalories.Add(counter, sum);
+                sum = 0;
+                counter++;
+            }
+        }
+        //Part 1
+        var max = ElfCalories.MaxBy(kv => kv.Value);
+        
+        return new($"{max.Value}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var lines = _input.Split('\n');
+        int sum = 0;
+        int counter = 1;
+        Dictionary<int, int> ElfCalories = new();
+
+        foreach (var line in lines)
+        {
+            if (int.TryParse(line, out int number))
+            {
+                sum += number;
+            }
+            else
+            {
+                ElfCalories.Add(counter, sum);
+                sum = 0;
+                counter++;
+            }
+        }
+
+        var sorted = ElfCalories.OrderBy(x => x.Value).Reverse().Take(3).ToList();
+        var total = sorted.Sum(x => x.Value);
+        return new($"{total}");
+    }
 }
